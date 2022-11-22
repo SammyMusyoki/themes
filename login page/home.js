650c3b107ad4c33c78bb47a2c27dcd087c893c41
sammy musyoki <75688249+SammyMusyoki@users.noreply.github.com>
2022-11-22 03:29:05 +0300

    first_commit.kenny

---
+++ login page/home.js
@@ -0,0 +1,8 @@
+function myLinks(){
+    var link = document.getElementById("nav_links");
+    if (link.style.display === "block"){
+        link.style.display = "none";
+    } else {
+        link.style.display = "block";
+    }
+}
